526e98a906ba9df875ab6afd8cc717a82d3a39f0
Camatoziz <vova.higer1@gmail.com>
2022-12-06 19:33:55 +0300

    JSNative-Homework-05.12

---
+++ src/01-hello-tests/01.test.tsx
@@ -0,0 +1,37 @@
+import {mult, sum} from './01';
+
+let a:number
+let b:number
+let c:number
+
+beforeEach(()=>{
+    a=1
+    b=2
+    c=3
+})
+
+test("Should be correct sum", ()=>{
+    //data
+
+    //action
+    const result1 = sum(a, b)
+    const result2 = sum(c, b)
+
+
+    //expect result
+    expect(result1).toBe(3)
+    expect(result2).toBe(5)
+})
+
+test("Should be correct multiply", ()=>{
+    //data
+
+    //action
+    const result1 = mult(a, b)
+    const result2 = mult(c, b)
+
+
+    //expect result
+    expect(result1).toBe(2)
+    expect(result2).toBe(6)
+})
